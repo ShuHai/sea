1d6e327a8efe112b4ed52123c5bb6d2c60535ea2
xiaowei.yang <xiaowei.yang@ele.me>
2018-03-27 14:28:04 +0800

    init

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -35,7 +35,7 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      '@': resolve('src')
     }
   },
   module: {
@@ -49,7 +49,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client'), resolve('packages')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
@@ -74,6 +74,10 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      {
+        test: /\.md$/,
+        loader: 'vue-markdown-loader'
       }
     ]
   },
---
+++ src/docs/a.md
@@ -0,0 +1,5 @@
+```html
+  <div>
+    <button type="success" show-icon>成功提示的文案</button>
+  </div>
+```
